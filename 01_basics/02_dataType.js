4afed4f19b0f65d19ff5fe72e61d080d799bda34
Atiqur_Rahman <mdatiqurrahmancse@gmail.com>
2025-07-29 13:55:42 +0600

    data Type

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict"  // treat all JS code as newer version
+
+// alert(3+3)  // we are using not js , not browser
+
+let name = "Atik"
+let age = 27
+let isLogging = false
+let state ;
+
+// name = 2 to power 53
+// bigint
+// string =  ""
+//  boolean = true / false
+// null = no valu pass
+// undefine = no define or not write
+// symbol = uniqe
+
+// object
+
+console.log(typeof age)
