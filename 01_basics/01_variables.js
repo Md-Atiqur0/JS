7c47f6e4e63d5f5856218d3d8920cf3a67a29a4c
Atiqur_Rahman <mdatiqurrahmancse@gmail.com>
2025-07-29 11:54:38 +0600

    variables of  js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "Atik@gmail.com"
+var accountPassword = "1234"
+accountCity = "Cumilla"
+let accountState ;
+
+// accountId = 2
+
+accountEmail = "rahman@gmail.com"
+accountPassword = "111"
+accountCity = "Dhaka"
+
+/*
+var not use
+*/
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
