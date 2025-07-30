f4dc8a61ee3ac96c625677a07a36214d91d6a679
Atiqur_Rahman <mdatiqurrahmancse@gmail.com>
2025-07-30 07:59:52 +0600

    string, Date, Math

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,24 @@
+const name = "Atiqur"
+const repoCount = 50
+
+//  console.log(name + repoCount + "Value");
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const myName = new String("Atiqur-Rahman")
+
+// console.log(myName[0]);
+// console.log(myName.length);
+// console.log(myName.toUpperCase());
+// console.log(myName.charAt(2));
+// console.log(myName.indexOf('i'));
+
+
+const newString = myName.substring(0, 5)
+console.log(newString);
+
+const anotherString = myName.slice(2, 5)
+console.log(anotherString);
+
+const newStringOne = "    Atiqur    "
+console.log(newStringOne.trim());
---
+++ 01_basics/07_date.js
@@ -0,0 +1,17 @@
+//  Date
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+let myCreateDate = new Date(2025, 8, 23)
+// console.log(myCreateDate.toLocaleString());
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+console.log(newDate.getMonth());
+console.log(newDate.getTime());
